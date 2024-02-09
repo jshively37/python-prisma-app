61414f2640de79b35e4c52148e545d4bd7598610
Jeff Shively <jshively@paloaltonetworks.com>
2024-02-09 09:39:09 -0500

    client.py framework

---
+++ prisma/client.py
@@ -0,0 +1,19 @@
+import requests
+import typing as t
+from urllib.error import HTTPError
+
+ENDPOINTS = {
+}
+
+PRISMA_HEADERS = {"Content-Type": "application/json"}
+
+
+class PelotonClient:
+    def __init__():
+        pass
+
+    def create_session(self) -> None:
+        pass
+
+    def make_request(self) -> t.Dict:
+        pass
