33fedfd752b7618404d395349f4b38740d19bbf5
Jeff Shively <jshively@paloaltonetworks.com>
2024-02-09 13:10:26 -0500

    Added in error checking on getting token

--- prisma/client.py
+++ prisma/client.py
@@ -1,6 +1,6 @@
-import requests
 import typing as t
-from urllib.error import HTTPError
+import requests
+import sys
 
 ENDPOINTS = {}
 GRANT_TYPE = "grant_type=client_credentials&scope=tsg_id"
@@ -24,14 +24,23 @@ class PrismaClient:
         print(PRISMA_HEADERS)
         self.session.headers = PRISMA_HEADERS
 
-    def _create_access_token(self):
+    def _create_access_token(self) -> None:
         url = "https://auth.apps.paloaltonetworks.com/oauth2/access_token"
         auth_data = f"{GRANT_TYPE}:{self.tsg_id}"
         auth_info = (self.client_id, self.client_secret)
-        response = requests.post(
-            url=url, headers=PRISMA_HEADERS, data=auth_data, auth=auth_info
-        ).json()
-        PRISMA_HEADERS.update({"Authorization": f"Bearer {response['access_token']}"})
+        try:
+            response = requests.post(
+                url=url, headers=PRISMA_HEADERS, data=auth_data, auth=auth_info
+            ).json()
+            PRISMA_HEADERS.update(
+                {"Authorization": f"Bearer {response['access_token']}"}
+            )
+        except KeyError:
+            sys.exit(
+                "Please validate your TSG, client id, or client secret and try again"
+            )
+        except Exception as e:
+            sys.exit(f"General error has occured: {e}")
 
     def make_request(self) -> t.Dict:
         pass
